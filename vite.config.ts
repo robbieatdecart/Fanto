73f330398d6ff25540a432f2d4b8beead3a543e2
robbieatdecart <robbie.khazan@decart.ai>
2025-06-05 21:08:47 +0300

    Simplify build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  envPrefix: 'VITE_',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'esbuild'
-  }
+  plugins: [react()]
 }) 
